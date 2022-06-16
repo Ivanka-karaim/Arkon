7bb0fcde238c9e4e18a7054553247aacdc8b9db1
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2022-06-16 22:44:24 +0300

    new migrate

--- database/migrations/2022_06_16_132936_create_data_people_table.php
+++ database/migrations/2022_06_16_132936_create_data_people_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('date_people', function (Blueprint $table) {
+        Schema::create('data_people', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('date_people');
+        Schema::dropIfExists('data_people');
     }
 };
